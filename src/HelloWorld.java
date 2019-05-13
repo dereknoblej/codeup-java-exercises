f0a233a595b1be730ba80f3e35ccb478a7df19ec
Derek Noble <dereknoble@Dereks-MacBook-Pro.local>
2019-05-13 13:38:19 -0500

    syntax exercise complete

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,59 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello, World!");
+
+        System.out.println("Hello World");
+        System.out.println("Test");
+
+//        int myFavoriteNumber = 17;
+//        System.out.println(myFavoriteNumber);
+//
+//        String myString = "Time to go to lunch.";
+//        System.out.println(myString);
+//
+//        myString = "c";
+//        System.out.println(myString);
+//
+//        float myNumber;
+//
+//        myNumber = 3.14F;
+//        System.out.println(myNumber);
+//
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+
+        int x = 10;
+        int y = 2;
+
+        x /= y;
+        y -= x;
+        System.out.println(y);
+
+
+
+//        int practiceInt = 128;
+//        long practiceNumber = practiceInt;
+//        short practiceShort = (short) practiceInt;
+//        practiceNumber += 1;
+//        System.out.println(practiceNumber);
+//
+//        float practiceFloat = 3.14F;
+//        System.out.println(practiceFloat);
+//        int floatCast = (int) practiceFloat;
+//        System.out.println(floatCast);
+//
+//        final int playableInt;
+//        playableInt = 7;
+
+
     }
 }
