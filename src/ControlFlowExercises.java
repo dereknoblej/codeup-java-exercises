fd5e7329dcba8a1671065c2ed5e28237d1cd15bf
Derek Noble <dereknoble@Dereks-MacBook-Pro.local>
2019-05-14 10:53:19 -0500

    table

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
 public class ControlFlowExercises {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 ////       int i = 5;
 ////        while (i <= 15){
 ////            System.out.println(i);
@@ -18,21 +20,31 @@ public class ControlFlowExercises {
 //        for(long x = 2; x < 1000000; x *= x){
 //            System.out.println(x);
 //        }
+//
+//    for(int i = 1; i <= 100; i++){
+//        if(i % 3 == 0 && i % 2 != 0){
+//            System.out.println("Fizz");
+//        }
+//        if(i % 2 == 0 && i % 3 != 0){
+//            System.out.println("Buzz");
+//        }
+//        if(i % 2 == 0 && i % 3 == 0){
+//            System.out.println("FizzBuzz");
+//        }
+//        System.out.println(i);
+//    }
+        System.out.println("Please enter a number");
+        int userNumber = sc.nextInt();
+        String leftAlignFormat = "| %-6d | %-6d |%-6d |%n";
 
-    for(int i = 1; i <= 100; i++){
-        if(i % 3 == 0 && i % 2 != 0){
-            System.out.println("Fizz");
+        System.out.format("+-----------------+------+%n");
+        System.out.format("| Number | Squared|Cubed |%n");
+        System.out.format("+-----------------+------+%n");
+        for (int i = 1; i <= userNumber; i++) {
+            int square = (int)Math.pow(i, 3);
+            System.out.format(leftAlignFormat, i, i * i, square);
         }
-        if(i % 2 == 0 && i % 3 != 0){
-            System.out.println("Buzz");
-        }
-        if(i % 2 == 0 && i % 3 == 0){
-            System.out.println("FizzBuzz");
-        }
-        System.out.println(i);
-    }
-
-
+        System.out.format("+-----------------+------+%n");
 
 
     }
