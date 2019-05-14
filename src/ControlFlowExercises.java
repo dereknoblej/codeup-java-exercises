65c285b203063fa3934475a615a4287a6a655b24
Derek Noble <dereknoble@Dereks-MacBook-Pro.local>
2019-05-14 10:13:51 -0500

    2

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,18 +1,23 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-       int i = 5;
-        while (i <= 15){
+//       int i = 5;
+//        while (i <= 15){
+//            System.out.println(i);
+//            i++;
+//        }
+        for (byte i = 5; i <= 15; i++){
             System.out.println(i);
-            i++;
         }
-        long x = 100;
-        do{
+//        long x = 2;
+//        do{
+//            System.out.println(x);
+//            x *= x;
+//
+//        } while (x < 1000000);
+
+        for(long x = 2; x < 1000000; x *= x){
             System.out.println(x);
-            x -= 5;
-
-        } while (x > -10);
-
-
+        }
 
 
 
