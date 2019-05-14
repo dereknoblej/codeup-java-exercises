f5b1731d4f590013d513bac19246770e2ca7bd1e
Derek Noble <dereknoble@Dereks-MacBook-Pro.local>
2019-05-14 10:25:49 -0500

    FizzBuzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,25 +1,36 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-//       int i = 5;
-//        while (i <= 15){
+////       int i = 5;
+////        while (i <= 15){
+////            System.out.println(i);
+////            i++;
+////        }
+//        for (byte i = 5; i <= 15; i++){
 //            System.out.println(i);
-//            i++;
 //        }
-        for (byte i = 5; i <= 15; i++){
-            System.out.println(i);
-        }
-//        long x = 2;
-//        do{
-//            System.out.println(x);
-//            x *= x;
+////        long x = 2;
+////        do{
+////            System.out.println(x);
+////            x *= x;
+////
+////        } while (x < 1000000);
 //
-//        } while (x < 1000000);
+//        for(long x = 2; x < 1000000; x *= x){
+//            System.out.println(x);
+//        }
 
-        for(long x = 2; x < 1000000; x *= x){
-            System.out.println(x);
+    for(int i = 1; i <= 100; i++){
+        if(i % 3 == 0 && i % 2 != 0){
+            System.out.println("Fizz");
         }
-
-
+        if(i % 2 == 0 && i % 3 != 0){
+            System.out.println("Buzz");
+        }
+        if(i % 2 == 0 && i % 3 == 0){
+            System.out.println("FizzBuzz");
+        }
+        System.out.println(i);
+    }
 
 
 
