4aa1322924a5ea97340f3a2cffd701fcbe68c3c0
Derek Noble <dereknoble@Dereks-MacBook-Pro.local>
2019-05-15 13:19:31 -0500

    Strings

---
+++ src/StringExercise.java
@@ -0,0 +1,13 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String message = "We don't need no education\n" +
+                "We don't need no thought control";
+        System.out.println(message);
+
+        message = message + "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(message);
+
+        message = message + "In windows, the main drive is usually C:\\";
+        System.out.println(message);
+    }
+}
