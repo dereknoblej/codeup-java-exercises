d90ccc7a75e9d1483b5ad3f0f4b30439e1dbfe86
Derek Noble <dereknoble@Dereks-MacBook-Pro.local>
2019-05-17 13:31:37 -0500

    TWo

--- src/Person.java
+++ src/Person.java
@@ -1,6 +1,10 @@
 public class Person {
     private String name;
 
+    public Person(String name){
+        this.name = name;
+    }
+
     public String getName(){
         return this.name;
     }
@@ -12,10 +16,20 @@ public class Person {
     }
 
     public static void main(String[] args) {
-        Person derek = new Person();
-        derek.setName("Derek");
-        derek.sayHello();
-        System.out.println(derek.getName());
+//        Person derek = new Person();
+//        derek.setName("Derek");
+//        derek.sayHello();
+//        System.out.println(derek.getName());
+
+        Person person1 = new Person("John");
+        Person person2 = person1;
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        person2.setName("Jane");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+
+
     }
 
 }
